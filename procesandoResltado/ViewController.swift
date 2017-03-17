d895d6f8c2b122b39cedd85421cea0380657c206
Rodrigo Guerra Castilla <geomin.rgc@gmail.com>
2017-03-17 15:29:19 -0600

    Initial Commit

---
+++ procesandoResltado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  procesandoResltado
+//
+//  Created by Rodrigo Guerra Castilla on 17/03/17.
+//  Copyright © 2017 Rodrigo Guerra Castilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
